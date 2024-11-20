5d58fb8949596be6470759f35e75adaf556ae034
Lwin Phyo <lwinphyo271395@gmail.com>
2024-11-20 15:46:35 +0700

    1st commit in git branch with computed function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,63 +1,35 @@
 <template>
   <div class="home">
-    
-    <h1>Person One</h1>
-    <p>{{ personOne.name }} {{ personOne.age }}</p>
-    <p>{{ personOne }}</p>
 
-    <button @click="changeOne">Click</button>
+    <input type="text" v-model="search">
+    <p>Search items - {{ search }}</p>
 
-    <h1>Person Two</h1>
-    <p>{{ personTwo.name }} {{ personTwo.age }}</p>
-    <p>{{ personTwo }}</p>
-
-    <button @click="changeTwo"> Change Two </button> <br><br>
-
-    <h3>user one = {{ user1 }} & user two = {{ user2 }}</h3>
-
-    <button @click="change1">ChangeOne</button> - 
-    <button @click="change2">ChangeTwo</button>
+    <div v-for="name in filteredNames" :key="name">
+      {{ name }}
+    </div>
 
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
-
-
+import { computed, ref } from 'vue';
 
 export default {
 
   setup(){
 
-    let personOne = ref({name:"kyaw",age:30});
+    let search = ref ("a")
 
-    let changeOne=()=>{
-      personOne.value.name = "Lwin";
-      // console.log(personOne.value.name)
-    }
+    let names = ref (["Lwin Phyo","Khaing Zar","Mya Phu","May Yamin"])
 
+    let filteredNames = computed (()=>{
+      return names.value.filter(name=>{
+        return name.includes(search.value)
+      })
+    })
 
-    let personTwo = reactive({name:"Maung", age:33});
-
-    let changeTwo = ()=>{
-      personTwo.name="Changed Maung"
-    }
-
-    let user1 = ref("Lwin Phyo")
-    let user2 = ref("Khaing Zar")
-
-    let change1 = ()=>{
-      user1.value="ChangeLwin"
-    }
-
-    let change2 = ()=>{
-      user2.value = "Change KZL"
-    }
-
-    return {personOne,changeOne,personTwo,changeTwo,user1,user2,change1,change2}
+    return {names,search,filteredNames};
 
   }
- 
 }
 </script>
