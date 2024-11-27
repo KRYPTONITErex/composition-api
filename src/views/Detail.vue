48c91438aa9b3294c3f6b2fce76f5acc5590ba6d
Lwin Phyo <lwinphyo271395@gmail.com>
2024-11-27 09:22:58 +0700

    Compo-5.1 - NavBar component and Detail style

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,7 +2,7 @@
 
   <!-- <h2>THIS IS DETAIL PAGE - {{ id }}</h2> -->
 
-  <div class="details" v-if="mypost">
+  <div class="post" v-if="mypost">
     <h2 class="lt">{{ mypost.title }}</h2>
     <p>{{ mypost.body }}</p>
   </div>
@@ -37,9 +37,53 @@ export default {
   align-items: center; /* Center horizontally */
   justify-content: center; /* Center vertically */
   text-align: center; /* Center text alignment */
-  min-height: 100vh; /* Fill the viewport vertically for centering */
+  min-height: auto; /* Fill the viewport vertically for centering */
   color: rgb(2, 34, 31);
   padding: 20px;
+}
+
+
+.post h2{
+    margin-top: 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    font-size: 26px;
+    color: azure;
+    margin: 30px;
+    max-width: 200px;
+}
+
+.post h2::before{
+    margin-left: 30px;
+    content: "";
+    display: block;
+    width: 130%;
+    height: 120%;
+    background: #ff8800;
+    position: absolute;
+    z-index: -1;
+    padding-right: 30px;
+    left: -50px;
+    transform: rotateZ(-5deg);
+    border-radius: 10px;
+}
+
+.pt{
+    padding-left: 15px;
+}
+
+.pill{
+    display: inline-block;
+    align-items: flex;
+    margin: 5px 5px 10px 0;
+    color: #094143;
+    background: #dafdf9;
+    padding: 8px;
+    border-radius: 10px;
+    font-size: 14px;
 }
 
 </style>
